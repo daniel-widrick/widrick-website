4db25eeae681d1000b861db5fdc8b85eb341f2f2
Daniel Widrick <dnaiel@widrick.net>
2025-03-12 21:18:21 -0400

    add more colors and ability to live test from windows

--- server.go
+++ server.go
@@ -7,23 +7,31 @@ import (
 	"strings"
 )
 
-func main(){
-	
+func main() {
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r * http.Request){
-		filePath := filepath.Join("./",r.URL.Path)
-		switch strings.TrimPrefix(filePath,"/") {
-		case "portfolio/software":
-			filePath = "./index.html"
-		case "portfolio/systems":
-			filePath = "./index.html"
-		case "portfolio/security":
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		filePath := filepath.Join("./", r.URL.Path)
+		normalizedPath := strings.ReplaceAll(strings.TrimPrefix(filePath, "/"), "\\", "/")
+		if strings.HasPrefix(normalizedPath, "portfolio/") {
+			validLocations := []string{"software", "systems", "security"}
+			isValid := false
+			for _, loc := range validLocations {
+				if strings.HasPrefix(normalizedPath, "portfolio/"+loc) {
+					isValid = true
+					break
+				}
+			}
+			if !isValid {
+				http.NotFound(w, r)
+				return
+			}
 			filePath = "./index.html"
 		}
-		log.Printf("%s  :: Serving file: %s (%s)\n",r.RemoteAddr,filePath, r.URL.Path)
+		log.Printf("%s  :: Serving file: %s (%s)\n", r.RemoteAddr, filePath, r.URL.Path)
 		http.ServeFile(w, r, filePath)
 	})
 
 	log.Println("Starting server on :6780")
-	log.Fatalln(http.ListenAndServe(":6780",mux))
-}	
+	log.Fatalln(http.ListenAndServe(":6780", mux))
+}
