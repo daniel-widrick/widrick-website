b933919b8c0b116debcc655220676275d9906a84
Daniel Widrick <dnaiel@widrick.net>
2025-04-20 13:18:45 -0400

    prevent directory traversal and clean up clunkiness

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,11 @@ COPY . .
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o portfolio
 
 FROM scratch
+WORKDIR /app
 COPY --from=builder /app/portfolio /portfolio
-COPY ./index.html /index.html
-COPY ./style.css /style.css
-COPY ./portfolio.js /portfolio.js
-COPY ./w-logo.svg /w-logo.svg
+COPY ./index.html ./index.html
+COPY ./style.css ./style.css
+COPY ./portfolio.js ./portfolio.js
+COPY ./w-logo.svg ./w-logo.svg
 
 ENTRYPOINT ["/portfolio"]
--- server.go
+++ server.go
@@ -11,22 +11,20 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		filePath := filepath.Join("./", r.URL.Path)
-		normalizedPath := strings.ReplaceAll(strings.TrimPrefix(filePath, "/"), "\\", "/")
-		if strings.HasPrefix(normalizedPath, "portfolio/") {
+		filePath := filepath.Join("/app", r.URL.Path)
+		log.Printf("%s  :: Request: %s (%s)\n", r.RemoteAddr, filePath, r.URL.Path)
+		if strings.HasPrefix(filePath, "/app/portfolio") {
 			validLocations := []string{"software", "systems", "security"}
-			isValid := false
 			for _, loc := range validLocations {
-				if strings.HasPrefix(normalizedPath, "portfolio/"+loc) {
-					isValid = true
+				locPath := filepath.Join("/app/portfolio", loc)
+				if strings.HasPrefix(filePath, locPath) {
+					filePath = "/app/index.html"
 					break
 				}
 			}
-			if !isValid {
-				http.NotFound(w, r)
-				return
-			}
-			filePath = "./index.html"
+		}
+		if filePath == "/app" {
+			filePath = "/app/index.html"
 		}
 		log.Printf("%s  :: Serving file: %s (%s)\n", r.RemoteAddr, filePath, r.URL.Path)
 		http.ServeFile(w, r, filePath)
